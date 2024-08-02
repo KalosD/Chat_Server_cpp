bfe847652708c2b4c3575d77db452533a3b6ddb6
du hongbo <dhb124@outlook.com>
2024-08-02 21:56:26 +0800

    初始化项目, 完成网络服务器模块和业务模块初步编写

---
+++ include/server/chatservice.hpp
@@ -0,0 +1,37 @@
+#ifndef CHATSERVICE_H
+#define CHATSERVICE_H
+
+#include <unordered_map>
+#include <functional>
+#include <muduo/net/TcpServer.h>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+using namespace muduo;
+using namespace muduo::net;
+using namespace std;
+
+// 处理消息的事件回调方法类型
+using MsgHandler = std::function<void(const TcpConnectionPtr &, json &, Timestamp)>;
+
+class ChatService
+{
+public:
+  // 获取单例模式对象的接口函数
+  static ChatService* instance();
+
+  // 处理登录业务
+  void login(const TcpConnectionPtr &conn, json &js, Timestamp time);
+  // 处理注册业务
+  void reg(const TcpConnectionPtr &conn, json &js, Timestamp time);
+  // 获取消息对应处理器
+  MsgHandler getHandler(int);
+private:
+  ChatService();
+  // 存储消息id和其对应的业务处理方法
+  unordered_map<int, MsgHandler> _msgHandlerMap;  // 消息id对应的处理操作
+
+};
+
+#endif
---
+++ src/server/chatserver.cpp
@@ -0,0 +1,44 @@
+#include "chatserver.hpp"
+#include "json.hpp"
+#include "chatservice.hpp"
+#include <functional>
+#include <string>
+using namespace std;
+using namespace placeholders;
+using json = nlohmann::json;
+
+ChatServer::ChatServer(EventLoop *loop, const InetAddress &listenAddr,
+             const string &nameArg) : _server(loop, listenAddr, nameArg), _loop(loop)
+{
+  // 注册连接回调
+  _server.setConnectionCallback(bind(&ChatServer::onConnection, this, _1));
+  // 注册读写事件回调
+  _server.setMessageCallback(bind(&ChatServer::onMessage, this, _1, _2, _3));
+  // 设置线程数量
+  _server.setThreadNum(4);
+}
+
+void ChatServer::start(){
+  _server.start();
+}
+
+
+// 上报链接相关信息的回调函数
+void ChatServer::onConnection(const TcpConnectionPtr &conn){
+  // 客户端断开连接
+  if(!conn->connected()){ 
+    conn->shutdown();
+  }
+}
+
+// 上报读写事件相关信息的回调函数
+void ChatServer::onMessage(const TcpConnectionPtr &conn, Buffer *buffer, Timestamp time){
+  string buf = buffer->retrieveAllAsString();
+  // 数据的反序列化：解码
+  json js = json::parse(buf);
+  // 目的：完全解耦网络模块和业务模块的代码
+  // 通过js["msgid"]获取=>业务handler=>conn js time
+  auto msgHandler = ChatService::instance()->getHandler(js["msgid"].get<int>());
+  msgHandler(conn, js, time);
+}
+
